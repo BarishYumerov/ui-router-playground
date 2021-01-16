2da69ddb8719d0daa006902bc0685db88ca4dd38
Barish Yumerov <barash@dsi.bg>
2021-01-17 00:14:30 +0200

    init and install ui-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UIRouterModule } from '@uirouter/angular';
+
 @NgModule({
   declarations: [
     AppComponent
